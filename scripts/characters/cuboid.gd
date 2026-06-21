c21cf8b5e1423a749852f966528329f1341dcebd
naton <nathan.chevenard@gmail.com>
2026-06-21 10:32:42 +0200

    Cuboid fix jump spam

--- scripts/characters/cuboid.gd
+++ scripts/characters/cuboid.gd
@@ -5,7 +5,9 @@ const gravity : float = -9.81
 
 var speed : float = 8
 var rotation_speed : float = 3
-var jump_force : float = 10
+var jump_force : float = 12
+
+var jump_colliding_bodies : Array[Node3D]
 
 
 func _physics_process(delta: float) -> void:
@@ -27,4 +29,23 @@ func _physics_process(delta: float) -> void:
 		angular_velocity.y = 0
 	
 	if Input.is_action_just_pressed("jump") && is_on_ground():
-		linear_velocity.y += jump_force
+		linear_velocity.y = jump_force
+
+
+func is_on_ground(checked_collisions : Array[PhysicsEntity] = []) -> bool:
+	checked_collisions.append(self)
+	
+	for body in jump_colliding_bodies:
+		if body.is_in_group("ground") || body.is_on_ground(checked_collisions):
+			return true
+	
+	return false
+
+
+func _on_jump_detection_area_3d_body_entered(body: Node3D) -> void:
+	if body != self:
+		jump_colliding_bodies.append(body)
+
+
+func _on_jump_detection_area_3d_body_exited(body: Node3D) -> void:
+	jump_colliding_bodies.erase(body)
