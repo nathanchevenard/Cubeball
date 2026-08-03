94d63fbf0a7e1e4558b874485a7c30710c38ae3c
naton <nathan.chevenard@gmail.com>
2026-08-03 15:56:27 +0200

    Ball spins based on linear_velocity

--- scripts/level_design/ball.gd
+++ scripts/level_design/ball.gd
@@ -4,6 +4,7 @@ class_name Ball
 @export var air_resistance : float = 0.1
 @export var friction : float = 10
 @export var maximum_speed : float = 50
+@export var spin_factor : float = 2
 
 var current_colliding_goal : Goal
 
@@ -20,6 +21,8 @@ func destroy():
 func _physics_process(delta: float) -> void:
 	var new_speed = linear_velocity.length() / (1 + linear_velocity.length() * air_resistance * delta)
 	linear_velocity = linear_velocity.normalized() * new_speed
+	
+	angular_velocity = linear_velocity.rotated(Vector3.UP, PI/2) * spin_factor
 	
 	if is_on_ground():
 		var normalized_velocity = linear_velocity.normalized()
