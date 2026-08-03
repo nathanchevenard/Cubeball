4426b72bad92c107164d6e4e38f2012f8be26bf6
naton <nathan.chevenard@gmail.com>
2026-08-03 13:14:18 +0200

    SettingsManager display FPS

--- scripts/managers/settings_manager.gd
+++ scripts/managers/settings_manager.gd
@@ -7,6 +7,9 @@ class_name SettingsManager
 @export var disable_goal_nets : bool = false
 @export var disable_cameras : bool = false
 
+@export var display_fps : bool
+@export var fps_label : Label
+
 static var instance : SettingsManager
 
 
@@ -22,6 +25,7 @@ func _ready() -> void:
 		disable_ui = true
 		disable_goal_nets = true
 		#disable_cameras = true
+		display_fps = false
 	
 	if disable_goal_animation == true:
 		SignalsManager.settings.emit_goal_animation_disable()
@@ -31,6 +35,14 @@ func _ready() -> void:
 		SignalsManager.settings.emit_goal_nets_disable()
 	if disable_cameras == true:
 		SignalsManager.settings.emit_cameras_disable()
+
+
+func _process(delta: float) -> void:
+	if display_fps == true:
+		fps_label.show()
+		fps_label.text = str(Performance.get_monitor(Performance.TIME_FPS)) + " FPS"
+	else:
+		fps_label.hide()
 
 
 func _on_all_teams_initialized():
